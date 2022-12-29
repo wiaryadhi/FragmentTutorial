d2b220cd4779f31334e05f620a022a31c7af22aa
ARY <widhiarya@outlook.com>
2022-12-29 13:58:52 +0700

    done pag

--- app/src/main/java/com/bcafinance/fragmenttutorial/MainActivity.kt
+++ app/src/main/java/com/bcafinance/fragmenttutorial/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.bcafinance.fragmenttutorial
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_login.textUserLog
+import kotlinx.android.synthetic.main.fragment_login.textPassLog
 
 class MainActivity : AppCompatActivity() {
+
+    var orientation: Int = 0;
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        orientation = resources.configuration.orientation
+
+        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+            loadFragment(LoginFragment.newInstance("", ""))
+
+//        btnRegistration.setOnClickListener(View.OnClickListener {
+//            loadFragment(Registration.newInstance("",""))
+//        })
+        }
     }
+
+    fun loadFragment(fragment: Fragment) {
+        val fragManager = supportFragmentManager.beginTransaction()
+        fragManager.replace(R.id.vFragment, fragment)
+        fragManager.addToBackStack("")
+        fragManager.commit()
+    }
+
+
 }
